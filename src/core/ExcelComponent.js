1e2e594d91cd8ce14661b8ff8317453758e41d01
Abdurasul <ahmedovabdurasul19@gmail.com>
2023-01-31 19:10:55 +0100

    finish base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -3,10 +3,26 @@ import { DomListener } from "./DomListener";
 export class ExcelComponent extends DomListener {
   constructor($root, option = {}){
     super($root, option.listeners)
+    this.name = option.name || ''
+    this.emitter = option.emitter
+    this.unsubsubscribes = []
+    this.prepare()
   }
+
+  prepare(){}
+
 	// Returns the template of the component
   toHTML() {
     return "";
+  }
+
+  $emit(event, ...args){
+    this.emitter.emit(event, ...args)
+  }
+
+  $on(event, fn){
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubsubscribes.push(unsub)
   }
 
   init(){
@@ -14,7 +30,7 @@ export class ExcelComponent extends DomListener {
   }
 
   destroy(){
-    console.log(this.$root);
     this.removeDOMListener()
+    this.unsubsubscribes.forEach(unsub => unsub())
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -11,56 +11,95 @@ class Dom {
       this.$el.innerHTML = html;
       return this;
     }
-		return this.$el.outerHTML.trim()
+    return this.$el.outerHTML.trim();
   }
 
-  clear(){
-		this.html("")
-		return this
-	}
-
-  on(eventType, callback){
-    this.$el.addEventListener(eventType, callback)
+  text(text) {
+    if (typeof text === "string") {
+      this.$el.textContent = text;
+      return this;
+    }
+    if(this.$el.tagName.toLowerCase() === "input"){
+      return this.$el.value.trim()
+    }
+    return this.$el.textContent.trim();
   }
 
-  of(eventType, callback){
-    this.$el.removeEventListener(eventType, callback)
+  clear() {
+    this.html("");
+    return this;
   }
 
-	append(node){
-		if(node instanceof Dom){
-			node = node.$el
-		}
-		if(Element.prototype.append){
-			this.$el.append(node)
-		}else{
-			this.$el.appendChild(node)
-		}
-		return this
-	}
-
-  get data(){
-    return this.$el.dataset
+  on(eventType, callback) {
+    this.$el.addEventListener(eventType, callback);
   }
 
-  closest(selector){
-    return $(this.$el.closest(selector))
+  of(eventType, callback) {
+    this.$el.removeEventListener(eventType, callback);
   }
 
-  getCords(){
-    return this.$el.getBoundingClientRect()
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el;
+    }
+    if (Element.prototype.append) {
+      this.$el.append(node);
+    } else {
+      this.$el.appendChild(node);
+    }
+    return this;
   }
 
-  findAll(selector){
-    return this.$el.querySelectorAll(selector)
+  get data() {
+    return this.$el.dataset;
   }
 
-  css(styles = {}){
-    Object.keys(styles).forEach(key => {
-      this.$el.style[key] = styles[key]
-    })
+  closest(selector) {
+    return $(this.$el.closest(selector));
   }
 
+  getCords() {
+    return this.$el.getBoundingClientRect();
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector);
+  }
+
+  find(selector) {
+    return $(this.$el.querySelector(selector));
+  }
+
+  addClass(className) {
+    return this.$el.classList.add(className);
+    return this
+  }
+  removeClass(className) {
+    return this.$el.classList.remove(className);
+    return this
+  }
+
+  id(parse) {
+    if (parse) {
+      const parsed = this.id().split(":");
+      return {
+        row: +parsed[0],
+        col: +parsed[1],
+      };
+    }
+    return this.data.id;
+  }
+
+  focus() {
+    this.$el.focus();
+    return this;
+  }
+
+  css(styles = {}) {
+    Object.keys(styles).forEach((key) => {
+      this.$el.style[key] = styles[key];
+    });
+  }
 }
 
 // event.target
